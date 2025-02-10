15c64fd2d6f329d928519b202eed287f16b57ad9
radiology111 <niuwenju@sxmu.edu.cn>
2025-02-10 22:34:41 +0800

    Delete CrossFormer directory

--- CrossFormer/CrossFormer/detection/dist_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CONFIG=$1
-GPUS=$2
-CHECKPOINT=$3
-
-PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS \
-    $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
